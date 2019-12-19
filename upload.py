feb7398f07cc924d5fe3813ba65e1c0bf2e29829
Gabriel <gabriel.smith01@gmail.com>
2019-12-19 10:39:46 -0500

    update logic on upload

--- upload.py
+++ upload.py
@@ -2,30 +2,63 @@ from cv2 import *
 import boto3
 import datetime
 import os
+import numpy as np
+import types
+import os
 from dotenv import load_dotenv
 load_dotenv()
-## check if datetime is a weekday between 7:30 - 5:30
+
+# check if datetime is a weekday between 7:30 - 5:30
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 today = datetime.datetime.now()
 
-if (today.weekday() <= 4 and today.hour > 7 and today.hour < 18 ):
-    cam = cv2.VideoCapture(0)   # 0 -> index of camera
-    client = boto3.client(
-        's3',
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        region_name='us-east-2'
-    )
+# TODO - add flag for testing: ignore time constraints
 
-    s, img = cam.read()
 
-    if s:
-        tstamp = today.strftime('%Y-%m-%d_%H-%M-%S')
-        filename = 'parking-vision-01-img_' + tstamp + '.jpg'
-        print('Saving Image');
-        cv2.imwrite(filename, img)
+def same_image(original, new):
+    if original.shape == new.shape:
+        print('The images have same size and channels')
+        difference = cv2.subtract(original, new)
 
-        print('Uploading Image')
+        b, g, r = cv2.split(difference)
+
+        if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
+            print('The images are completely equal')
+            return True
+
+    return False
+
+
+# if today.weekday() <= 4 and 7 < today.hour < 18:
+cam = cv2.VideoCapture(0)
+success, img = cam.read()
+success = True
+duplicate_image = False
+
+if success:
+    previous_image = cv2.imread('previous.jpg')
+
+    tstamp = today.strftime('%Y-%m-%d_%H-%M-%S')
+    filename = 'parking-vision-01-img_' + tstamp + '.jpg'
+    print('Saving Image')
+    cv2.imwrite(filename, img)
+
+    if previous_image is not None:
+        print('previous image found, comparing')
+        duplicate_image = same_image(cv2.imread(filename), previous_image)
+
+
+    if duplicate_image is False:
+        print('Images are different, uploading new image')
+        client = boto3.client(
+            's3',
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            region_name='us-east-2'
+        )
         client.upload_file(filename, 'parking-vision', filename)
+
+        print('renaming image to previous')
+        os.rename(filename, 'previous.jpg')
