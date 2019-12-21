028d64befcdf847dbd1912049bf420dbf5282a6c
Gabriel <gabriel.smith01@gmail.com>
2019-12-21 16:48:32 -0500

    updated compare and upload to use new compare functions

--- compare.py
+++ compare.py
@@ -1,6 +1,5 @@
 # import the necessary packages
 from skimage.metrics import structural_similarity as ssim
-import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
@@ -8,12 +7,12 @@ def mse(imageA, imageB):
 	# the 'Mean Squared Error' between the two images is the
 	# sum of the squared difference between the two images;
 	# NOTE: the two images must have the same dimension
-	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-	err /= float(imageA.shape[0] * imageA.shape[1])
+	result = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+	result /= float(imageA.shape[0] * imageA.shape[1])
 
 	# return the MSE, the lower the error, the more "similar"
 	# the two images are
-	return err
+	return result
 
 def compare_images(imageA, imageB, title):
 	# compute the mean squared error and structural similarity
@@ -23,50 +22,18 @@ def compare_images(imageA, imageB, title):
 
 	print("%s: MSE: %.2f, SSIM: %.2f" % (title, m, s))
 
-	# setup the figure
-	# fig = plt.figure(title)
-	# plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
-
-	# # show first image
-	# ax = fig.add_subplot(1, 2, 1)
-	# plt.imshow(imageA, cmap = plt.cm.gray)
-	# plt.axis("off")
-
-	# # show the second image
-	# ax = fig.add_subplot(1, 2, 2)
-	# plt.imshow(imageB, cmap = plt.cm.gray)
-	# plt.axis("off")
-
-	# # show the images
-	# plt.show()
-
 # load the images -- the original, the original + contrast,
 # and the original + photoshop
-original = cv2.imread("images/parking-vision-01-img_2019-12-19_09-51-21.jpg")
+different = cv2.imread("images/parking-vision-01-img_2019-12-20_07-53-55.jpg")
 repeat = cv2.imread("images/parking-vision-01-img_2019-12-19_10-01-21.jpg")
-contrast = cv2.imread("images/parking-vision-01-img_2019-12-19_10-21-59.jpg")
+original = cv2.imread("images/parking-vision-01-img_2019-12-19_10-21-59.jpg")
 
 # convert the images to grayscale
 original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 repeat = cv2.cvtColor(repeat, cv2.COLOR_BGR2GRAY)
-contrast = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
-
-# initialize the figure
-fig = plt.figure("Images")
-images = ("Original", original), ("Contrast", contrast), ("Repeat", repeat)
-
-# loop over the images
-# for (i, (name, image)) in enumerate(images):
-# 	# show the image
-# 	ax = fig.add_subplot(1, 3, i + 1)
-# 	ax.set_title(name)
-# 	plt.imshow(image, cmap = plt.cm.gray)
-# 	plt.axis("off")
-
-# # show the figure
-# plt.show()
+different = cv2.cvtColor(different, cv2.COLOR_BGR2GRAY)
 
 # compare the images
 compare_images(original, original, "Original vs. Original")
 compare_images(original, repeat, "Original vs. Repeat")
-compare_images(original, contrast, "Original vs. Contrast")
+compare_images(original, different, "Original vs. Different")
--- upload.py
+++ upload.py
@@ -1,10 +1,19 @@
+from skimage.metrics import structural_similarity as ssim
+import numpy as np
 import cv2
 import boto3
 import datetime
 import os
-import numpy as np
 import types
 import os
+import argparse
+
+# Get command line arguments to force run
+parser = argparse.ArgumentParser(description='Take picture and upload')
+parser.add_argument('-f', action='store_true', help='Force run image and upload')
+args = parser.parse_args()
+force = args.f
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -16,49 +25,54 @@ today = datetime.datetime.now()
 
 # TODO - add flag for testing: ignore time constraints
 
+def mse(image_a, image_b):
+    # the 'Mean Squared Error' between the two images is the
+	# sum of the squared difference between the two images;
+	# NOTE: the two images must have the same dimension
+    result = np.sum((image_a.astype('float') - image_b.astype('float')) ** 2)
+    result /= float(image_a.shape[0] * image_b.shape[1])
+
+    return result
+
 
 def same_image(original, new):
-    if original.shape == new.shape:
-        print('The images have same size and channels')
-        difference = cv2.subtract(original, new)
+    mse_result = mse(original, new)
+    ssim_result = ssim(original, new)
 
-        b, g, r = cv2.split(difference)
+    # MSE increases the images are less similar
+    # SSIM has smaller values, which indicate less similarity.
 
-        if cv2.countNonZero(b) == 0 and cv2.countNonZero(g) == 0 and cv2.countNonZero(r) == 0:
-            print('The images are completely equal')
-            return True
-
-    return False
+    return ssim_result >= 0.75 and mse_result <= 1000
 
 
-# if today.weekday() <= 4 and 7 < today.hour < 18:
-cam = cv2.VideoCapture(0)
-success, img = cam.read()
-success = True
-duplicate_image = False
+if (today.weekday() <= 4 and 7 < today.hour < 18) or force:
+    cam = cv2.VideoCapture(0)
+    success, img = cam.read()
+    success = True
+    duplicate_image = False
 
-if success:
-    previous_image = cv2.imread('previous.jpg')
+    if success:
+        previous_image = cv2.imread('previous.jpg')
 
-    tstamp = today.strftime('%Y-%m-%d_%H-%M-%S')
-    filename = 'parking-vision-01-img_' + tstamp + '.jpg'
-    print('Saving Image')
-    cv2.imwrite(filename, img)
+        tstamp = today.strftime('%Y-%m-%d_%H-%M-%S')
+        filename = 'parking-vision-01-img_' + tstamp + '.jpg'
+        print('Saving Image')
+        cv2.imwrite(filename, img)
 
-    if previous_image is not None:
-        print('previous image found, comparing')
-        duplicate_image = same_image(cv2.imread(filename), previous_image)
+        if previous_image is not None:
+            print('previous image found, comparing')
+            is_duplicate = same_image(cv2.imread(filename), previous_image)
 
 
-    if duplicate_image is False:
-        print('Images are different, uploading new image')
-        client = boto3.client(
-            's3',
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            region_name='us-east-2'
-        )
-        client.upload_file(filename, 'parking-vision', filename)
+        if is_duplicate is False:
+            print('Images are different, uploading new image')
+            client = boto3.client(
+                's3',
+                aws_access_key_id=AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                region_name='us-east-2'
+            )
+            client.upload_file(filename, 'parking-vision', filename)
 
-        print('renaming image to previous')
-        os.rename(filename, 'previous.jpg')
+            print('renaming image to previous')
+            os.rename(filename, 'previous.jpg')
